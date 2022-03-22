e50f12a0daa4955a2289ce74e408600a8bde4495
Erick Guzman <guzmi.dev@gmail.com>
2022-03-21 19:36:28 -0700

    :bug: const in components

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas/widgets/widgets.dart';
 
 class DetailsScreen extends StatelessWidget {
    
@@ -9,10 +10,19 @@ class DetailsScreen extends StatelessWidget {
     // TODO: Cambier luego por una instancia de mavie
   final String movie = ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
 
-    return const Scaffold(
+    return  Scaffold(
       body: CustomScrollView(
         slivers: [
-          _CustomAppBar()
+          const _CustomAppBar(),
+          SliverList(
+            delegate: SliverChildListDelegate([
+             const _PosterAndTitle(),
+             const _Overview(),
+             const _Overview(),
+             const _Overview(),
+             const CastingCards()
+            ])
+          )
         ],
       ),
     );
@@ -33,6 +43,7 @@ class _CustomAppBar extends StatelessWidget {
         centerTitle: true,
         titlePadding: const EdgeInsets.all(0),
         title: Container(
+          padding: const EdgeInsets.only(bottom: 10),
           width: double.infinity,
           alignment: Alignment.bottomCenter,
           color: Colors.black12,
@@ -49,6 +60,73 @@ class _CustomAppBar extends StatelessWidget {
           fit: BoxFit.cover,
           ),
       ),
+    );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  const _PosterAndTitle({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: const FadeInImage(
+              placeholder: AssetImage('assets/no-image.jpg'), 
+              image: NetworkImage('https://via.placeholder.com/200x300'),
+              height: 150,
+            ),
+          ),
+          const SizedBox(width: 20),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'movie.title', 
+                style: Theme.of(context).textTheme.headline5,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+              Text(
+                'movie.originalTitle', 
+                style: Theme.of(context).textTheme.subtitle1,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 1,
+              ),
+              Row(
+                children: [
+                  const Icon(Icons.star_outline, size: 15, color: Colors.grey),
+                  const SizedBox(width: 5,),
+                  Text('movie.voteAverage', style: Theme.of(context).textTheme.caption,)
+                ],
+                
+              )
+            ],
+          )
+        ],
+      ),
+      
+    );
+  }
+}
+
+class _Overview extends StatelessWidget {
+  const _Overview({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+      child: Text(
+        "Excepteur minim occaecat sunt ullamco adipisicing velit fugiat ea qui qui. Laborum ullamco nulla cupidatat pariatur dolore incididunt ut non amet mollit nulla aliquip. Irure laborum qui fugiat cupidatat. Do eu magna ad elit eu incididunt est aliquip qui labore excepteur aliquip.",
+        textAlign: TextAlign.justify,
+        style: Theme.of(context).textTheme.subtitle1,
+        ),
     );
   }
 }
