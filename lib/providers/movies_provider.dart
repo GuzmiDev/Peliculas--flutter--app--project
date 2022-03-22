7fd70b4cc0780f2d440fc57335b59a90b8090844
Erick Guzman <guzmi.dev@gmail.com>
2022-03-22 16:01:43 -0700

    :recycle: funcion privada en movies_provider. Optimizacion del codigo

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -11,36 +11,35 @@ class MoviesProvider extends ChangeNotifier {
   final String _languague = 'es-ES';
   List<Movie> onDisplayMovies = [];
   List<Movie> popularMovies = [];
+  int _popularPage = 0;
 
   MoviesProvider(){
     getOnDisplayMovies();
     getPopularMovies();
   }
 
-
-  getOnDisplayMovies() async{
-    var url = Uri.https(_baseUrl, '/3/movie/now_playing', {
+  Future<String> _getJsonData(String endpoint, [int page = 1]) async {
+    var url = Uri.https(_baseUrl, endpoint, {
       'api_key': _apiKey,
       'languague' : _languague,
-      'page': '1'
-      });
-
+      'page': '$page'
+    });
     final response = await http.get(url);
-    final nowPlayingResponse = NowPlayingResponse.fromJson(response.body);
+    return response.body;
+  }
+
+
+  getOnDisplayMovies() async{
+    final jsonData = await _getJsonData('/3/movie/now_playing');
+    final nowPlayingResponse = NowPlayingResponse.fromJson(jsonData);
     onDisplayMovies = nowPlayingResponse.results;
     notifyListeners();
   }
   
   getPopularMovies() async{
-    var url = Uri.https(_baseUrl, '/3/movie/popular', {
-      'api_key': _apiKey,
-      'languague' : _languague,
-      'page': '1'
-      });
-
-    final response = await http.get(url);
-    final popularResponse = PopularResponse.fromJson(response.body);
-
+    _popularPage++;
+    final jsonData = await _getJsonData('/3/movie/popular', _popularPage);
+    final popularResponse = PopularResponse.fromJson(jsonData);
     popularMovies = [...popularMovies,...popularResponse.results];
     notifyListeners();
   }
